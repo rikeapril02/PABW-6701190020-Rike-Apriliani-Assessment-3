e0eb467ccccffb0fee0901a585cc9d9b49d77d14
rikeapril02 <71260031+rikeapril02@users.noreply.github.com>
2021-06-25 20:47:46 +0700

    Update Database.php

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -34,7 +34,7 @@ class Database extends Config
 		'DSN'      => '',
 		'hostname' => 'localhost',
 		'username' => 'root',
-		'password' => 'doeraso387',
+		'password' => '',
 		'database' => 'ci4_4',
 		'DBDriver' => 'MySQLi',
 		'DBPrefix' => '',
